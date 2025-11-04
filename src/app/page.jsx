6d9195607cf3aaf8be8c699320aad1f84afb15a0
Maty1ek <galymzakangerov@icloud.com>
2025-11-04 19:08:20 +0800

    bg circles fix attempt 3 or 4

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
 import bgCircles from "../../public/bg_circles.svg";
 import { CircleUserRound, Menu } from "lucide-react";
+import MainInputs from "@/components/main-inputs";
 
 export default function Home() {
   return (
-    <div className="">
-      <div className="flex flex-col justify-center pt-[18px] items-center  pb-[30px] relative">
-        <div className="absolute flex justify-center top-[-18px] h-full w-full overflow-hidden">
-          <div className="  min-w-[760px] z-0">
+      <div className="flex flex-col justify-center pt-[18px] items-center h-[100dvh] relative overflow-hidden">
+        <div className="absolute flex justify-center top-[-18px] h-full min-h-[750px]  w-full ">
+          <div className="  min-w-[760px] ">
             <Image src={bgCircles} width={760} height={2000} className=" " />
           </div>
         </div>
 
-        <div className="mainCon z-10 flex flex-col px-[20px] items-center w-full min-w-[308px] max-w-[500px] pt-[15px] h-[100vh]">
+        <div className="mainCon z-10 flex flex-col px-[20px] items-center w-full min-w-[308px] max-w-[500px] pt-[15px] h-full pb-[40px]">
           <div className="heading_buttons w-full px-[0px] flex justify-between text-white ">
             <div className="burger_button">
               <Menu size={36} />
@@ -32,53 +32,12 @@ export default function Home() {
             </p>
           </div>
 
-          <div className="main_input_con py-[22px] px-[20px] mt-[40px] w-full bg-[#fafafa3a] rounded-[15px]">
-            <div className=" text-[#414141]">
-              <h1 className="font-extrabold text-[24px]">
-                Countries and Years
-              </h1>
-              <p className="mt-[5px] ml-[1px] text-[15px]">
-                Enter the countries you've lived in and the number of years you
-                resided in each.
-              </p>
-            </div>
-
-            <div className="flex flex-col justify-center">
-              <div className="flex mt-[25px]">
-                <div className="w-[50%] font-bold text-[18px] text-[#414141]">
-                  <h3>Countries</h3>
-                </div>
-                <div className="w-[50%] font-bold text-[18px] text-[#414141]">
-                  <h3>Years</h3>
-                </div>
-              </div>
-
-              <div className="flex my-[10px]">
-                <div className="w-[50%]">
-                  <input
-                    type="text"
-                    className="bg-white w-[120px] h-[23px] rounded-[5px]"
-                  />
-                </div>
-                <div className="w-[50%]">
-                  <input
-                    type="text"
-                    className="bg-white w-[120px] h-[23px] rounded-[5px]"
-                  />
-                </div>
-              </div>
-
-              <button className="bg-[#fff] rounded-[6px] h-[30px] w-[135px] font-bold text-[15px] mt-[20px]">
-                Add country +
-              </button>
-            </div>
-          </div>
+          <MainInputs/>
 
           <button className="submit_button mt-[40px] color-[#0f0f0f] text-[26px] font-bold w-full rounded-[14px] bg-white h-[50px]">
             SUBMIT
           </button>
         </div>
       </div>
-    </div>
   );
 }
---
+++ src/components/main-inputs.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const MainInputs = () => {
+  return (
+    <div className="main_input_con py-[22px] px-[20px] mt-[40px] w-full bg-[#fafafa3a] rounded-[15px]">
+            <div className=" text-[#414141]">
+              <h1 className="font-extrabold text-[24px]">
+                Countries and Years
+              </h1>
+              <p className="mt-[5px] ml-[1px] text-[15px]">
+                Enter the countries you've lived in and the number of years you
+                resided in each.
+              </p>
+            </div>
+
+            <div className="flex flex-col justify-center">
+              <div className="flex mt-[25px]">
+                <div className="w-[50%] font-bold text-[18px] text-[#414141]">
+                  <h3>Countries</h3>
+                </div>
+                <div className="w-[50%] font-bold text-[18px] text-[#414141]">
+                  <h3>Years</h3>
+                </div>
+              </div>
+
+              <div className="flex my-[10px]">
+                <div className="w-[50%]">
+                  <input
+                    type="text"
+                    className="bg-white w-[120px] h-[23px] rounded-[5px]"
+                  />
+                </div>
+                <div className="w-[50%]">
+                  <input
+                    type="text"
+                    className="bg-white w-[120px] h-[23px] rounded-[5px]"
+                  />
+                </div>
+              </div>
+
+              <button className="bg-[#fff] rounded-[6px] h-[30px] w-[135px] font-bold text-[15px] mt-[20px]">
+                Add country +
+              </button>
+            </div>
+          </div>
+  )
+}
+
+export default MainInputs
