ed99043035e794da45853478d96c0e0bcdb77291
Maty1ek <galymzakangerov@icloud.com>
2025-12-17 18:17:49 +0800

    fixed search paras thing agains complete version 1.0 prod yuhoo yeeheeey

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -345,7 +345,7 @@ export default function Home() {
 
   return (
     <div className="relative min-h-screen flex justify-center pt-[15px]">
-      <Suspense>
+      <Suspense fallback={null}>
         <SearchParamsHandler
           onPaymentSuccess={() => {
             setIsPollingPayment(true);
